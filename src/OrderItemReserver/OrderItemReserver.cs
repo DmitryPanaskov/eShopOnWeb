7dfd4a4ea60afd172d0637aa29c7a7a9ded69b84
DmitryPanaskov <44893730+DmitryPanaskov@users.noreply.github.com>
2023-09-25 11:11:16 +0200

    final task

---
+++ src/OrderItemReserver/OrderItemReserver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace OrderItemReserver
+{
+    public class OrderItemReserver
+    {
+        private const string connectionStringBlob = "";
+
+        [FunctionName("OrderItemReserver")]
+        public async Task Run([ServiceBusTrigger("service-bus-queus-dp", Connection = "AzureWebJobsService")]string myQueueItem, ILogger log)
+        {
+            Console.WriteLine("Strat");
+            log.LogInformation("Strat DeliveryOrderProcessor");
+
+            var blobServiceClient = new BlobServiceClient(connectionStringBlob);
+            var containerName = "orderitems";
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var fileName = "item_" + Guid.NewGuid().ToString() + ".json";
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(GenerateStringFromString(myQueueItem));
+
+            log.LogInformation("Done DeliveryOrderProcessor");
+            Console.WriteLine("Done");
+        }
+
+        private Stream GenerateStringFromString(string myQueueItem)
+        {
+            var stream = new MemoryStream();
+            var write = new BinaryWriter(stream);
+            write.Write(myQueueItem);   
+            write.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
--- src/Web/Pages/Basket/Checkout.cshtml.cs
+++ src/Web/Pages/Basket/Checkout.cshtml.cs
@@ -1,4 +1,9 @@
-﻿using Ardalis.GuardClauses;
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Ardalis.GuardClauses;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +13,8 @@ using Microsoft.eShopWeb.ApplicationCore.Exceptions;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.Infrastructure.Identity;
 using Microsoft.eShopWeb.Web.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Microsoft.eShopWeb.Web.Pages.Basket;
 
@@ -20,18 +27,21 @@ public class CheckoutModel : PageModel
     private string? _username = null;
     private readonly IBasketViewModelService _basketViewModelService;
     private readonly IAppLogger<CheckoutModel> _logger;
+    private readonly IConfiguration _configuration;
 
     public CheckoutModel(IBasketService basketService,
         IBasketViewModelService basketViewModelService,
         SignInManager<ApplicationUser> signInManager,
         IOrderService orderService,
-        IAppLogger<CheckoutModel> logger)
+        IAppLogger<CheckoutModel> logger,
+        IConfiguration configuration)
     {
         _basketService = basketService;
         _signInManager = signInManager;
         _orderService = orderService;
         _basketViewModelService = basketViewModelService;
         _logger = logger;
+        _configuration = configuration;
     }
 
     public BasketViewModel BasketModel { get; set; } = new BasketViewModel();
@@ -53,8 +63,17 @@ public class CheckoutModel : PageModel
             }
 
             var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
+            var address = new Address("123 Main St.", "Kent", "OH", "United States", "44240");
+
+            var item = new Item();
+            item.Adress = address;
+            item.ListOfItems = items.ToList();
+            item.FinalPrice = items.Select(x => x.UnitPrice).Sum().ToString();
+            await WriteToCosmoDb(item);
+            await WriteToAzureServiceBys(items);
+
             await _basketService.SetQuantities(BasketModel.Id, updateModel);
-            await _orderService.CreateOrderAsync(BasketModel.Id, new Address("123 Main St.", "Kent", "OH", "United States", "44240"));
+            await _orderService.CreateOrderAsync(BasketModel.Id, address);
             await _basketService.DeleteBasketAsync(BasketModel.Id);
         }
         catch (EmptyBasketOnCheckoutException emptyBasketOnCheckoutException)
@@ -66,6 +85,82 @@ public class CheckoutModel : PageModel
 
         return RedirectToPage("Success");
     }
+
+    private async Task WriteToCosmoDb(Item item)
+    {
+        HttpClient client = new HttpClient();
+        var url = _configuration.GetValue<string>("AzureFunctions:OrderItemReserver");
+        var myCoutent = JsonConvert.SerializeObject(item);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(myCoutent);
+        var byteContent = new ByteArrayContent(buffer);
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        _logger.LogInformation($"Start request to function {url}");
+        var response =await client.PostAsync(url, byteContent).ConfigureAwait(false);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            // Do something with respoonse.
+        }
+
+    }
+
+    private async Task WriteToAzureServiceBys(IEnumerable<BasketItemViewModel> items)
+    {
+        ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        ServiceBusSender sender;
+
+        // number of messages to be sent to the queue
+        const int numOfMessages = 3;
+
+        // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when messages are being published or read
+        // regularly.
+        //
+        // Set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
+        // If you use the default AmqpTcp, ensure that ports 5671 and 5672 are open.
+        var clientOptions = new ServiceBusClientOptions
+        {
+            TransportType = ServiceBusTransportType.AmqpWebSockets
+        };
+        //TODO: Replace the "<NAMESPACE-NAME>" and "<QUEUE-NAME>" placeholders.
+        client = new ServiceBusClient(
+            "eshoponweb-dp.servicebus.windows.net",
+            new DefaultAzureCredential(),
+            clientOptions);
+        sender = client.CreateSender("service-bus-queues-dp");
+
+        // create a batch 
+        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+        for (int i = 1; i <= numOfMessages; i++)
+        {
+            // try adding a message to the batch
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+            {
+                // if it is too large for the batch
+                throw new Exception($"The message {i} is too large to fit in the batch.");
+            }
+        }
+
+        try
+        {
+            // Use the producer client to send the batch of messages to the Service Bus queue
+            await sender.SendMessagesAsync(messageBatch);
+            Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        Console.WriteLine("Press any key to end the application");
+        Console.ReadKey();
+    }
+
 
     private async Task SetBasketModelAsync()
     {
@@ -93,5 +188,14 @@ public class CheckoutModel : PageModel
         var cookieOptions = new CookieOptions();
         cookieOptions.Expires = DateTime.Today.AddYears(10);
         Response.Cookies.Append(Constants.BASKET_COOKIENAME, _username, cookieOptions);
+    }
+
+    private class Item
+    {
+        public Address Adress { get; set; }
+
+        public List<BasketItemViewModel> ListOfItems { get; set; }
+
+        public string FinalPrice { get; set; }
     }
 }
